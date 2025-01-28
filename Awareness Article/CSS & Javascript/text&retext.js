0ba784a2853e99f79f33af9ed82c74d89f84d891
tech-man07 <jagangamer1502@gmail.com>
2025-01-28 16:06:45 +0530

    Add files via upload

---
+++ Awareness Article/CSS & Javascript/text&retext.js
@@ -0,0 +1,20 @@
+function text()
+    {
+    const photo=document.getElementById('photo');
+    photo.style.width="0px"
+    photo.style.height="0px"
+    const image=document.getElementById('deepak');
+    document.body.style.backgroundImage="url('disco.gif')";
+    deepak.style.display="block";
+    }
+
+
+function retext()
+{
+ const photo=document.getElementById('photo');
+    photo.style.width="80px";
+    photo.style.height="90px";
+    const deepak=document.getElementById('deepak');
+    document.body.style.backgroundImage="none";
+    deepak.style.display="none";
+}
